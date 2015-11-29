dffe18e0048bf19417cdc72477da91cb0d3b7fdf
Terry Gliedt <tpg@hps.com>
2015-11-29 13:39:47 -0500

    Copy remapped build 37 BAM to NCBI

---
+++ bin/topmed_ncbib37.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+#
+#   topmed_ncbib37.sh -submit bamid
+#
+#	Send the proper set of files to NCBI for the remap b37 bam
+#
+bindir=/usr/cluster/bin
+samtools=/net/mario/gotcloud/bin/samtools
+ascpcmd="$bindir/ascp -i /net/topmed/incoming/study.reference/send2ncbi/topmed-2-ncbi.pri -l 800M -k 1"
+ascpdest='asp-um-sph@gap-upload.ncbi.nlm.nih.gov:protected'
+topmedcmd=/usr/cluster/monitor/bin/topmedcmd.pl
+topmedxml=/usr/cluster/monitor/bin/topmed_xml.pl
+mem=8G
+console=/net/topmed/working/topmed-output
+topmeddir=/net/topmed/incoming/topmed
+remapdir=/net/topmed/working/schelcj/results
+
+if [ "$1" = "-submit" ]; then
+  shift
+  #   May I submit this job?
+  $topmedcmd permit test sb37 $1
+  if [ "$?" = "0" ]; then
+    exit 4
+  fi 
+
+  #   This will usually have files on both topmeds, so it does not matter where it runs
+  l=(`$topmedcmd where $1`)     # Get bampath backuppath bamname realhost realhostindex
+  realhost="${l[3]}"
+  realhostindex="${l[4]}"
+  slurmp="$realhost-incoming"
+  slurmqos="$realhost-ncbi"
+
+  #  Submit this script to be run
+  l=(`/usr/cluster/bin/sbatch -p $slurmp --mem=$mem --qos=$slurmqos --workdir=$console -J $1-ncbiorig --output=$console/$1-ncbiorig.out $0 $*`)
+  if [ "$?" != "0" ]; then
+    echo "Failed to submit command to SLURM"
+    echo "CMD=/usr/cluster/bin/sbatch -p $slurmp --mem=$mem --qos=$slurmqos --workdir=$console -J $1-ncbiorig --output=$console/$1-ncbiorig.out $0 $*"
+    exit 1
+  fi
+  $topmedcmd mark $1 sentorig submitted
+  if [ "${l[0]}" = "Submitted" ]; then      # Job was submitted, save jobid
+    echo `date` ncbiorig ${l[3]} >> $console/$1.jobids
+  fi
+  exit
+fi
+
+if [ "$1" = "" ]; then
+  me=`basename $0`
+  echo "Usage: $me [-submit] bamid nwdid center path2originalbamfile"
+  echo ""
+  echo "Send original BAM files to NCBI"
+  exit 1
+fi
+bamid=$1
+shift
+nwdid=`$topmedcmd show $bamid expt_sampleid`
+if [ "$nwdid" = "" ]; then
+  echo "Invalid bamid '$bamid'. NWDID not known"
+  $topmedcmd mark $bamid sentb37 failed
+  exit 2
+fi
+center=`$topmedcmd show $bamid center`
+if [ "$center" = "" ]; then
+  echo "Invalid bamid '$bamid'. CENTER not known"
+  $topmedcmd mark $bamid sentb37 failed
+  exit 2
+fi
+piname=`$topmedcmd show $bamid piname`
+if [ "$piname" = "" ]; then
+  echo "Invalid bamid '$bamid'. PINAME not known"
+  $topmedcmd mark $bamid sentb37 failed
+  exit 2
+fi
+fqremappedcram="$remapdir/$center/$piname/$nwdid/bams/$nwdid.recal.cram"
+if [ ! -f "$fqremappedcram" ]; then
+  echo "Remapped CRAM for bamid '$bamid' ($fqremappedcram) not found"
+  $topmedcmd mark $bamid sentb37 failed
+  exit 2
+fi
+
+d=`date +%Y/%m/%d`
+echo "#========= $d $SLURM_JOB_ID $0 bamid=$bamid file=$fqremappedcram ========="
+#   Go to our working directory
+cd $console
+if [ "$?" != "0" ]; then
+  echo "Unable to CD to '$console' to create XML file"
+  $topmedcmd mark $bamid sentb37 failed
+  exit 2
+fi
+mkdir XMLfiles 2>/dev/null
+cd XMLfiles
+here=`pwd`
+
+#   Create the BAM file to be sent
+#   samtools view by default writes to std out.  Could use option "-o *.bam" to
+#   suppress this and specify the output file name in the options to samtools.
+#   Option "-@" tells how many extra threads to use for compression when writing
+#   a .bam file.  I'm just guessing 3 -- meaning 4 threads total, including the main one.
+sendbam="$nwdid.recal.bam"
+echo "Creating BAM $sendbam from CRAM $fqremappedcram"
+stime=`date +%s`
+$samtools view -b -@3 -o $sendbam $fqremappedcram 
+if [ "$?" != "0" ]; then
+  echo "Unable to create BAM: $samtools .. $sendbam $fqremappedcram"
+  $topmedcmd mark $bamid sentb37 failed
+  exit 2
+fi
+etime=`date +%s`
+etime=`expr $etime - $stime`
+echo "BAM created from CRAM in $etime seconds"
+
+#   Calculate the MD5 for the bam
+echo "Calculating MD5 for new bam"
+stime=`date +%s`
+checksum=`md5sum $sendbam | awk '{print $1}'`
+if [ "$checksum" = "" ]; then
+  echo "Unable to calculate the MD5 for the new BAM: md5sum $sendbam"
+  $topmedcmd mark $bamid sentb37 failed
+  exit 3
+fi
+etime=`date +%s`
+etime=`expr $etime - $stime`
+echo "MD5 calculated from CRAM in $etime seconds"
+
+#   Create the XML to be sent
+$topmedxml -xmlprefix $here/ -type remap $bamid $here/$sendbam $checksum
+if [ "$?" != "0" ]; then
+  echo "Unable to create remap run XML files"
+  $topmedcmd mark $bamid sentb37 failed
+  exit 2
+fi
+
+#   Check files we created.  Note hardcoded 37 here. Fix later
+files=''
+for f in $nwdid-remap.37.submit.xml $nwdid-remap.37.run.xml; do
+  if [ ! -f $f ]; then
+    echo "Missing XML file '$f'"   
+    $topmedcmd mark $bamid sentb37 failed
+    exit 2
+  fi
+  files="$files $f"
+done
+
+echo "Sending data file to NCBI - $sendbam"
+ls -l $sendbam
+stime=`date +%s`
+$ascpcmd $sendbam $ascpdest
+rc=$?
+rm -f $sendbam
+if [ "$rc" != "0" ]; then
+  echo "FAILED to send data file '$sendbam'"
+  $topmedcmd mark $bamid sentb37 failed
+  exit 2
+fi
+etime=`date +%s`
+etime=`expr $etime - $stime`
+echo "BAM file sent in $etime seconds"
+
+echo "Sending XML files to NCBI - $files"
+$ascpcmd $files $ascpdest
+if [ "$?" = "0" ]; then
+  echo "XML files '$files' sent to NCBI"
+  $topmedcmd mark $bamid sentb37 delivered
+  exit
+fi
+
+echo "FAILED to send data XML files to NCBI - $files"
+$topmedcmd mark $bamid sentb37 failed 
+exit 1
+
+
+
+
