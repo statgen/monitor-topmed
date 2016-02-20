4e8db1b1fa61004d9f00462449e866995116e8e9
Terry Gliedt <tpg@hps.com>
2016-02-20 10:15:59 -0500

    Rewrite code to always keep SLURM nodes up

--- bin/topmed_drained.sh
+++ bin/topmed_drained.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 #
-#   topmed_drained.sh
+#   topmed_drained.sh hostname
 #
 #	SLURM gets drained too frequently so we check it's state
 #   every now and then and resume it
 #   This script must be run by a user with SLURM admin rights
 #
-host=topmed
-state=`/usr/cluster/bin/sstate | grep $host | grep DRAINED`
-if [ "$state" = "DRAINED" ]; then
-  echo "Resuming DRAINED SLURM daemon"
-  scontrol update NodeName=$host State=RESUME
+hosts=topmed
+if [ "$1" != "" ]; then
+  hosts="$*"
 fi
+for host in $hosts; do
+  state=`/usr/cluster/bin/sinfo -p ${host}-incoming show node | grep drain`
+  if [ "$state" != "" ]; then
+    echo "Resuming DRAINED SLURM daemon at '$host' (state=$state)"
+    scontrol update nodename=$host State=RESUME
+    /usr/cluster/bin/sinfo -p ${host}-incoming show node
+  #else 
+    #echo "Node '$host' does not need to be resumed"
+    #/usr/cluster/bin/sinfo -p ${host}-incoming show node
+  fi
+done
