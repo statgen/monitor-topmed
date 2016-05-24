39dfcf88c0fff6c8d09407ddac9c0b1f4b61178e
Terry Gliedt <tpg@hps.com>
2016-05-24 14:01:46 -0400

    Force incoming data to be writable

---
+++ bin/topmedforcew.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+#   topmedforcew.sh dir
+#
+#	Force in incoming directory to be writable so the monitor can deal with it
+
+#   We only play with files in /incoming/topmed
+topdir=/incoming/topmed
+topuser=topmed
+a=`echo $1 | grep $topdir`
+if [ "$a" = "" ]; then
+  echo "$0 $topdir/center/run"
+  echo ""
+  echo "Force directories in $topdir to be writable for user $topuser"
+  exit 1
+fi
+
+cd $1
+if [ "$?" != "0" ]; then
+  echo "$0 - Unable to CD to '$1'"
+  exit 4
+fi
+echo "Forcing '$1' to be writable for the monitor"
+chgrp topmed .
+chmod 770 .
+chmod 664 Manifest.txt *.md5 2> /dev/null   # Failure is OK
