68b71e0c804c1a3a2db286a8a7c0db73d9e8dc2b
Terry Gliedt <tpg@hps.com>
2016-06-07 14:19:39 -0400

    A smarter way to check if a CRAM has been finished yet

--- bin/topmed_calcmd5.sh
+++ bin/topmed_calcmd5.sh
@@ -2,14 +2,26 @@
 #
 #   topmed_calcmd5.sh - calculate the md5 for a bam or cram
 #
+#   You might find a truncated cram for testing in
+#       /net/topmed/incoming/study.reference/study.reference/NWD850567.trunc.test.cram
+#
 #   If the file has not completed (no end of file marker, yet)
 #   we wait a bit and try again, failing after a few minutes of this.
 #
 #	prints md5sum checksum to stdout
+if [ "$1" = "" ]; then
+  echo "$0  cram"
+  echo "Use this in other scripts to calculate the md5 for a cram"
+  echo "It will attempt to wait until the cram is complete before trying"
+  exit 1
+fi
+
 samtools=/usr/cluster/bin/samtools
+awkcalcend=/usr/cluster/monitor/bin/topmed_calc_cram_end.awk
 
 for n in 1 2 3 4 5 6 7 8 9; do
-  l=`$samtools view $1 1:3,000,000-3,010,000 2>&1 | grep truncate`
+  offset=`samtools view -H $1 | awk -f $awkcalcend`
+  l=`$samtools view $1 $offset 2>&1 | grep truncate`
   if [ "$l" = "" ]; then
     md5sum $1               # Let caller trap results
     exit $?
