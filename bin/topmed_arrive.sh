e1b853a82db886055d8e00820f5c72f53210eedd
Terry Gliedt <tpg@hps.com>
2016-07-26 15:07:55 -0400

    Add -persist for topmedcmd.pl calls, determine build dynamically

--- bin/topmed_arrive.sh
+++ bin/topmed_arrive.sh
@@ -9,7 +9,7 @@
 topmedcmd=/usr/cluster/monitor/bin/topmedcmd.pl
 topmednwd=/usr/cluster/monitor/bin/topmed_nwdid.pl
 topmedrename=/usr/cluster/monitor/bin/topmedrename.pl
-console=/net/topmed/working/topmed-output
+samtools=/net/mario/gotcloud/bin/samtools
 
 #   We do not expect -submit to be used here
 if [ "$1" = "-submit" ]; then
@@ -30,6 +30,28 @@ bamfile=$2
 #   Mark this as started
 $topmedcmd mark $bamid arrived started || exit $?
 
+#   Determine build used to generate this bam/cram
+build=''
+l=(`$samtools view -H $bamfile | grep '@SQ' | cut -f 1-3 | head -2`)
+if [ "${l[2]}" = "LN:249250621" -a "${l[5]}" = "LN:243199373" ]; then
+  build=37
+else
+  if [ "${l[2]}" = "LN:248956422" -a "${l[5]}" = "LN:242193529" ]; then
+    build=38
+  else
+    a=`echo $1 | grep illumina`
+    if [ "$a" != "" ]; then       # Path has illumina in it
+      build=37
+    fi
+  fi
+fi
+if [ "$build" = "" ]; then
+  echo "Unable to determine build for '$bamfile' (L2=${l[2]} L5=${l[5]}"
+  $topmedcmd mark $bamid arrived failed
+  exit 4
+
+fi
+
 #   Set NWDID and other database fields
 $topmednwd -bamid $bamid $bamfile
 rc=$?
@@ -42,8 +64,8 @@ fi
 $topmedrename $bamid $bamfile
 rc=$?
 if [ "$rc" != "0" ]; then
-  $topmedcmd mark $bamid arrived failed
+  $topmedcmd -persist mark $bamid arrived failed
   exit $rc
 fi
-$topmedcmd mark $bamid arrived completed
+$topmedcmd -persist mark $bamid arrived completed
 exit 0
