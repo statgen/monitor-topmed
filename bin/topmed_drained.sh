4c81b797650b90789adb4ff05068a79e3f03357e
Terry Gliedt <tpg@hps.com>
2016-10-18 21:51:21 -0400

    Rewrite to make smaller and easier to change

--- bin/topmed_drained.sh
+++ bin/topmed_drained.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 #
-#   topmed_drained.sh hostname
+#   topmed_drained.sh hostname hostname2 ...
 #
 #	SLURM gets drained too frequently so we check it's state
 #   every now and then and resume it
 #   This script must be run by a user with SLURM admin rights
 #
-hosts=topmed
-if [ "$1" != "" ]; then
-  hosts="$*"
-fi
-for host in $hosts; do
-  state=`/usr/cluster/bin/sinfo -p ${host}-incoming show node | grep drain`
+
+for host in $*; do
+  state=`/usr/cluster/bin/scontrol show node $host | grep State= | grep DRAIN`
   if [ "$state" != "" ]; then
-    echo "Resuming DRAINED SLURM daemon at '$host' (state=$state)"
-    scontrol update nodename=$host State=RESUME
-    /usr/cluster/bin/sinfo -p ${host}-incoming show node
-  #else 
-    #echo "Node '$host' does not need to be resumed"
-    #/usr/cluster/bin/sinfo -p ${host}-incoming show node
+    #echo "Resuming DRAINED SLURM daemon at '$host'"
+    scontrol update nodename=$host state=RESUME 2>/dev/null
   fi
 done
