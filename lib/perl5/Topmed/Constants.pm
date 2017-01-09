745b8edb1d17d107c6840922e22f1e3bc0beafbc
Chris Scheller <schelcj@pobox.com>
2017-01-09 09:56:17 -0500

    some default constants plus the google buckets

--- lib/perl5/Topmed/Constants.pm
+++ lib/perl5/Topmed/Constants.pm
@@ -5,12 +5,75 @@ use Readonly;
 
 use Topmed::Base;
 
-our @EXPORTS = ();
+our @EXPORT = (
+  qw(
+    $EMPTY
+    $COMMA
+    $UNDERSCORE
+    $PERIOD
+    $TRUE
+    $FALSE
+    $PIPE
+    $DASH
+    $SPACE
+    $TAB
+    $COLON
+    $SLASH
+    $MAX_DELAY
+    $TIMEZONE
+    )
+);
 
-our @EXPORT_OK = ();
+our @EXPORT_OK = (
+  qw(
+    $EMPTY
+    $COMMA
+    $UNDERSCORE
+    $PERIOD
+    $TRUE
+    $FALSE
+    $PIPE
+    $DASH
+    $SPACE
+    $TAB
+    $COLON
+    $SLASH
+    $MAX_DELAY
+    $TIMEZONE
+    %GOOGLE_BUCKETS
+    )
+);
 
 our %EXPORT_TAGS = (
-  all => [],
+  all    => \@EXPORT_OK,
+  google => [
+    qw(
+      %GOOGLE_BUCKETS
+      )
+  ],
+);
+
+Readonly::Scalar our $EMPTY      => q{};
+Readonly::Scalar our $COMMA      => q{,};
+Readonly::Scalar our $UNDERSCORE => q{_};
+Readonly::Scalar our $PERIOD     => q{.};
+Readonly::Scalar our $TRUE       => q{1};
+Readonly::Scalar our $FALSE      => q{0};
+Readonly::Scalar our $PIPE       => q{|};
+Readonly::Scalar our $DASH       => q{-};
+Readonly::Scalar our $SPACE      => q{ };
+Readonly::Scalar our $TAB        => qq{\t};
+Readonly::Scalar our $COLON      => q{:};
+Readonly::Scalar our $SLASH      => q{/};
+Readonly::Scalar our $TIMEZONE   => q{America/Detroit};
+
+Readonly::Hash our %GOOGLE_BUCKETS => (
+  incoming => 'gs://topmed-incoming',
+  fastqs   => 'gs://topmed-fastqs',
+  crams    => 'gs://topmed-crams',
+  recabs   => 'gs://topmed-recabs',
+  backups  => 'gs://topmed-backup-%s',
+  logs     => 'gs://topmed-logs',
 );
 
 1;
