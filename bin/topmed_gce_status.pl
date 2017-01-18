053caa7b65854ceed0a1b7f512a242222611a0c0
Chris Scheller <schelcj@pobox.com>
2017-01-18 11:00:38 -0500

    not needed

--- bin/topmed_gce_status.pl
+++ bin/topmed_gce_status.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 
 use FindBin;
-use lib (qq($FindBin::Bin/../local/lib/perl5), qq($FindBin::Bin/../lib/perl5),);
+use lib (qq($FindBin::Bin/../local/lib/perl5), qq($FindBin::Bin/../lib/perl5));
 
 use Topmed::Base qw(cmds);
 use Topmed::Constants qw(:states);
