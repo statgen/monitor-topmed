a4db66ecf7b63017b723a39cd676fe1b0a4be91d
Terry Gliedt <tpg@hps.com>
2017-01-25 08:39:47 -0500

    New production path means changed paths

--- bin/topmed_fixit.pl
+++ bin/topmed_fixit.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -I/usr/cluster/lib/perl5/site_perl -I/usr/cluster/monitor/lib/perl5 -I /usr/cluster/monitor/bin
+#!/usr/bin/perl
 ###################################################################
 #
 # Name: topmed_fixit.pl
@@ -16,9 +16,14 @@
 use strict;
 use warnings;
 use FindBin qw($Bin $Script);
-use lib "$FindBin::Bin";
-use lib "$FindBin::Bin/../lib";
-use lib "$FindBin::Bin/../lib/perl5";
+use lib (
+  qq($FindBin::Bin),
+  qq($FindBin::Bin/../lib),
+  qq($FindBin::Bin/../lib/perl5),
+  qq($FindBin::Bin/../local/lib/perl5),
+  qq(/usr/cluster/topmed/lib/perl5),
+  qq(/usr/cluster/topmed/local/lib/perl5),
+);
 use My_DB;
 use TopMed_Get;
 use Getopt::Long;
@@ -35,11 +40,11 @@ my $COMPLETED = 20;           # Task completed successfully
 my $CANCELLED = 89;           # Task cancelled
 my $FAILED    = 99;           # Task failed
 
-my $topmedbin = '/usr/cluster/monitor/bin';
+my $topmedbin = '/usr/cluster/topmed/bin';
 our %opts = (
     topmedcmd => "$topmedbin/topmedcmd.pl",
     topmedcmdnwd => "$topmedbin/topmed_nwdid.pl",
-    realm => '/usr/cluster/monitor/etc/.db_connections/topmed',
+    realm => '/usr/cluster/topmed/etc/.db_connections/topmed',
     centers_table => 'centers',
     runs_table => 'runs',
     studies_table => 'studies',
