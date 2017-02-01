52f84cc85cd0a2d400dcf2558f2013c87aac984b
Terry Gliedt <tpg@hps.com>
2017-02-01 12:04:15 -0500

    First real run of post processing of pull sample

---
+++ bin/topmed_gcepost.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+#
+#   topmed_gcepost.sh -submit| bamid
+#
+#	Post-process a remapped CRAM after being pulled from Google Cloud
+#
+. /usr/cluster/topmed/bin/topmed_actions.inc
+vt=/net/wonderland/home/lefaivej/bin/vt
+
+me=gcepost
+mem=2G
+markverb="${me}ed"
+constraint="--constraint eth-10g"
+qos="--qos=topmed-$me"
+slurmp=topmed
+realhost=''
+gsutil='gsutil -o GSUtil:parallel_composite_upload_threshold=150M'
+incominguri='gs://topmed-recabs'
+build=38
+
+if [ "$1" = "-submit" ]; then
+  shift
+  #   May I submit this job?
+  $topmedcmd permit test $me $1
+  if [ "$?" = "0" ]; then
+    exit 4
+  fi 
+
+  # Run this on node where remapped cram lives
+  h=`$topmedpath whathost $1 b$build`
+  if [ "$h" != "" ]; then
+    realhost="--nodelist=$h"
+    #qos="--qos=$h-$me"
+  fi
+
+  #  Submit this script to be run
+  l=(`/usr/cluster/bin/sbatch -p $slurmp --mem=$mem $realhost $constraint $qos --workdir=$console -J $1-$me --output=$console/$1-$me.out $0 $sq $*`)
+  if [ "$?" != "0" ]; then
+    echo "Failed to submit command to SLURM"
+    echo "CMD=/usr/cluster/bin/sbatch -p $slurmp --mem=$mem $realhost $constraint $qos --workdir=$console -J $1-$me --output=$console/$1-$me.out $0 $sq $*"
+    exit 1
+  fi
+  $topmedcmd mark $1 $markverb submitted
+  if [ "${l[0]}" = "Submitted" ]; then      # Job was submitted, save job details
+    echo `date` $me ${l[3]} $slurmp $slurmqos $mem >> $console/$1.jobids
+  fi
+  exit
+fi
+
+if [ "$1" = "" ]; then
+  me=`basename $0`
+  echo "Usage: $me [-submit] bamid"
+  echo ""
+  echo "Post-process a remapped CRAM after being pulled from Google Cloud"
+  exit 1
+fi
+bamid=$1
+
+d=`date +%Y/%m/%d`
+s=`hostname`
+p=`pwd`
+
+crampath=`$topmedpath wherepath $bamid b$build`
+if [ "$crampath" = "" ]; then
+  echo "Unable to determine where remapped CRAM file should go for '$bamid'"
+  $topmedcmd -persist mark $bamid $markverb failed
+  exit 2
+fi
+echo "#========= '$d' host=$s $SLURM_JOB_ID $0 bamid=$bamid crampath=$crampath pwd=$p ========="
+
+#======================================================================
+#   Post process the remapped CRAM from Google Cloud
+#======================================================================
+#   Mark this as started
+$topmedcmd mark $bamid $markverb started
+
+#   CD to remapped cram file location
+cd $crampath
+if [ "$?" != "0" ]; then
+  echo "Unable to CD to directory for remapped CRAM file '$bamid'"
+  $topmedcmd -persist mark $bamid $markverb failed
+  exit 2
+fi
+
+nwdid=`$topmedcmd show $bamid expt_sampleid`
+if [ "$nwdid" = "" ]; then
+  echo "Unable to get expt_sampleid for bamid '$bamid'"
+  $topmedcmd -persist mark $bamid $markverb failed
+  exit 2
+fi
+piname=`$topmedcmd show $bamid piname`
+if [ "$piname" = "" ]; then
+  echo "Unable to get piname for bamid '$bamid'"
+  $topmedcmd -persist mark $bamid $markverb failed
+  exit 2
+fi
+
+#   Start by looking for file in GCE.  If there, get the MD5 for it and compare with ours
+stime=`date +%s`
+cramfile=$nwdid.recab.cram
+
+if [ ! -f $cramfile ]; then
+  echo "Unable to find the remapped data for '$bamid' [$nwdid] cramfile=$cramfile"
+  pwd
+  $topmedcmd -persist mark $bamid $markverb failed
+  exit 2
+fi
+
+export BOTO_CONFIG=/net/topmed/working/shared/tpg_gsutil_config.txt
+gce_checksum="$(gsutil stat $incominguri/$nwdid/$cramfile |grep 'md5'|awk {'print $3'}|base64 -d|hexdump -ve '/1 "%02x"')"
+if [ $gce_checksum != "" ]; then        # File does exist in gcepost.  Check MD5
+    echo "Calculating MD5 for local file ($cramfile)"
+    md5=(`md5sum $cramfile`)
+    md5=${md5[0]}
+    if [ "$md5" != "$gce_checksum" ]; then
+        echo "Local file ($cramfile) checksum ($md5) does not match $gce_checksum for bamid '$bamid' [$nwdid]"
+        $topmedcmd -persist mark $bamid $markverb failed
+        exit 3
+    fi
+    echo "MD5 for local and GCE file match ($md5)"
+fi
+
+#   Create crai for cram
+echo "Creating index file '$cramfile'"
+$samtools index $cramfile 2>&1
+if [ "$?" != "0" ]; then
+  echo "Unable to create index file for bamid '$bamid' [$nwdid]"
+  $topmedcmd -persist mark $bamid $markverb failed
+  exit 2
+fi
+echo "Created CRAI file for bamid '$bamid' [$nwdid]"
+
+#   Create the BCF file
+bcfdir=`$topmedpath wherepath $bamid bcf`
+mkdir -p $bcfdir
+if [ "$?" != "0" ]; then
+  echo "Unable to create BCF output directory for '$bamid' [$nwdid] - $bcfdir"
+  $topmedcmd -persist mark $bamid $markverb failed
+  exit 2
+fi
+bcffile=$bcfdir/$nwdid.bcf
+
+$vt discover -b $cramfile -s $nwdid -r /net/topmed/working/mapping/gotcloud/ref/hg38/hs38DH.fa -o $bcffile
+if [ "$?" != "0" ]; then
+  echo "Unable to run VT DISCOVER for bamid '$bamid' [$nwdid] on $cramfile creating $bcffile"
+  $topmedcmd -persist mark $bamid $markverb failed
+  exit 2
+fi
+bcftools index $bcffile
+if [ "$?" != "0" ]; then
+  echo "Unable to run BCFTOOLS for bamid '$bamid' [$nwdid] on $bcffile"
+  $topmedcmd -persist mark $bamid $markverb failed
+  exit 2
+fi
+
+etime=`date +%s`
+etime=`expr $etime - $stime`
+echo "Post processing of remapped CRAM ($crampath) completed in $etime seconds"
+$topmedcmd -persist mark $bamid $markverb completed
+$topmedcmd -persist mark $bamid mapped$build completed
+$gsutil rm $incominguri/$nwdid/$cramfile.flagstat
+$gsutil rm $incominguri/$nwdid/$cramfile
+echo `date` $me $SLURM_JOB_ID ok $etime secs >> $console/$bamid.jobids
+exit
