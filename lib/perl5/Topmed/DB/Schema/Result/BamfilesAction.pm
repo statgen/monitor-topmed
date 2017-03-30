2fea46bd34803161f83c1ee51257a49355ec31dd
Chris Scheller <schelcj@pobox.com>
2017-03-30 15:49:18 -0400

    our verison of mysql does not support CURRENT_TIMESTAMP for a default on DATETIME fields so use this plugin to set it at insert time

--- lib/perl5/Topmed/DB/Schema/Result/BamfilesAction.pm
+++ lib/perl5/Topmed/DB/Schema/Result/BamfilesAction.pm
@@ -186,4 +186,5 @@ __PACKAGE__->belongs_to(
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
+__PACKAGE__->load_components("InflateColumn::CreatedAt");
 1;
