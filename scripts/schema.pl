2b0cac8ad57a7d4830f17d8999a339e2035b9bf6
Terry Gliedt <tpg@hps.com>
2017-03-30 16:38:12 -0400

    Commit so build behaves with new schema.pl

--- scripts/schema.pl
+++ scripts/schema.pl
@@ -6,7 +6,7 @@ use lib (qq($Bin/../lib/perl5), qq($Bin/../local/lib/perl5));
 use DBIx::Connector;
 use DBIx::Class::Schema::Loader qw(make_schema_at);
 
-my $ctx        = DBIx::Connector->new(realm => 'topmed');
+my $ctx        = DBIx::Connector->new(realm => 'topmed'); 
 my $realm      = $ctx->read_realm_file();
 my $contraints = join(
   '|', (
