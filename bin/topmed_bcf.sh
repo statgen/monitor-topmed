885da911f9ab9ddedf42b583ff36e6bc6c1ba1a9
Terry Gliedt <tpg@hps.com>
2017-04-25 05:04:10 -0400

    Use Bash functions

--- bin/topmed_bcf.sh
+++ bin/topmed_bcf.sh
@@ -11,39 +11,14 @@ bcftools=/usr/cluster/bin/bcftools
 bam=/usr/cluster/bin/bam
 
 me=bcf
-mem=12G
 markverb=$me
-qos="--qos=topmed-$me"
-build=38
 
 if [ "$1" = "-submit" ]; then
   shift
-  #   May I submit this job?
-  $topmedpermit permit test $me $1
-  if [ "$?" = "0" ]; then
-    echo "$me $1 not permitted" | tee $console/$1-$me.out
-    exit 4
-  fi 
-
-  # Run this on node where remapped cram lives 
-  h=`$topmedpath whathost $1 b$build`
-  if [ "$h" != "" ]; then
-    realhost="--nodelist=$h"
-    #qos="--qos=$h-$me"
-  fi
-
-  #  Submit this script to be run
-  l=(`/usr/cluster/bin/sbatch -p $slurmp --mem=$mem $qos --workdir=$console -J $1-$me --output=$console/$1-$me.out $0 $sq $*`)
-  if [ "$?" != "0" ]; then
-    $topmedcmd -emsg "Failed to submit command to SLURM - $l" mark $1 $markverb failed
-    echo "Failed to submit command to SLURM - $l" > $console/$1-$me.out
-    echo "CMD=/usr/cluster/bin/sbatch -p $slurmp --mem=$mem $realhost $qos --workdir=$console -J $1-$me --output=$console/$1-$me.out $0 $sq $*" >> $console/$1-$me.out
-    exit 1
-  fi
-  $topmedcmd mark $1 $markverb submitted
-  if [ "${l[0]}" = "Submitted" ]; then      # Job was submitted, save job details
-    echo `date` $me ${l[3]} $slurmp $slurmqos $mem >> $console/$1.jobids
-  fi
+  bamid=`$topmedcmd show $1 bamid`
+  MayIRun $me $bamid
+  MyRealHost $bamid b$build
+  SubmitJob $bamid "$topmed-$me" '16G' "$0 $*"
   exit
 fi
 
@@ -56,40 +31,27 @@ if [ "$1" = "" ]; then
 fi
 bamid=$1
 
-d=`date +%Y/%m/%d`
-s=`hostname`
-p=`pwd`
+Started
+GetNWDID $bamid
 stime=`date +%s`
-
 crampath=`$topmedpath wherepath $bamid b$build`
 if [ "$crampath" = "" ]; then
-  $topmedcmd -persist -emsg "Unable to determine where remapped CRAM file should be for '$bamid'" mark $bamid $markverb failed
-  exit 2
+  Fail  "Unable to determine where remapped CRAM file should be for '$bamid'"
 fi
-echo "#========= '$d' host=$s $SLURM_JOB_ID $0 bamid=$bamid crampath=$crampath pwd=$p ========="
 
 #======================================================================
 #   Post process the remapped CRAM from Google Cloud
 #======================================================================
-#   Mark this as started
-$topmedcmd mark $bamid $markverb started
-
 #   CD to remapped cram file location
 cd $crampath
 if [ "$?" != "0" ]; then
-  $topmedcmd -persist emsg "Unable to CD to directory for remapped CRAM file '$bamid'" mark $bamid $markverb failed
+  $topmedcmd -persist emsg "Unable to CD to directory for remapped CRAM file '$bamid'"
   exit 2
 fi
 
-nwdid=`$topmedcmd show $bamid expt_sampleid`
-if [ "$nwdid" = "" ]; then
-  $topmedcmd -persist -emsg "Unable to get expt_sampleid for bamid '$bamid'" mark $bamid $markverb failed
-  exit 2
-fi
 cramfile=`$topmedpath wherefile $bamid b$build`
 if [ ! -f $cramfile ]; then
-  $topmedcmd -persist -emsg "Unable to find remapped cram file '$cramfile'" mark $bamid $markverb failed
-  exit 2
+  Fail "Unable to find remapped cram file '$cramfile'"
 fi
 
 #   Create crai for cram if it does not exist
@@ -98,8 +60,7 @@ if [ ! -f $crai ]; then
   echo "Creating index file '$cramfile'"
   $samtools index $cramfile 2>&1
   if [ "$?" != "0" ]; then
-    $topmedcmd -persist -emsg "Unable to create index file for bamid '$bamid' [$nwdid]" mark $bamid $markverb failed
-    exit 2
+    Fail "Unable to create index file for bamid '$bamid' [$nwdid]"
   fi
   echo "Created CRAI file for bamid '$bamid' [$nwdid]"
 fi
@@ -108,26 +69,28 @@ fi
 bcfdir=`$topmedpath wherepath $bamid bcf`
 mkdir -p $bcfdir
 if [ "$?" != "0" ]; then
-  $topmedcmd -persist -emsg "Unable to create BCF output directory for '$bamid' [$nwdid] - $bcfdir" mark $bamid $markverb failed
-  exit 2
+  Fail "Unable to create BCF output directory for '$bamid' [$nwdid] - $bcfdir"
 fi
 bcffile=$bcfdir/$nwdid.bcf
 
 set -o pipefail
 $samtools view -uh $cramfile | $bam clipOverlap --poolSize 100000000 --in -.ubam --out -.ubam | $vt discover2 -z -q 20 -b + -r $vtref -s $nwdid -o $bcffile
 if [ "$?" != "0" ]; then
-  $topmedcmd -persist -emsg "Unable to run VT DISCOVER for bamid '$bamid' [$nwdid] on $cramfile creating $bcffile" mark $bamid $markverb failed
-  exit 2
+  Fail "Unable to run VT DISCOVER for bamid '$bamid' [$nwdid] on $cramfile creating $bcffile"
 fi
 $bcftools index $bcffile
 if [ "$?" != "0" ]; then
-  $topmedcmd -persist -emsg "Unable to run BCFTOOLS for bamid '$bamid' [$nwdid] on $bcffile" mark $bamid $markverb failed
-  exit 2
+  Fail "Unable to run BCFTOOLS for bamid '$bamid' [$nwdid] on $bcffile"
 fi
 
 etime=`date +%s`
 etime=`expr $etime - $stime`
 echo "Created BCF file for remapped CRAM ($crampath) completed in $etime seconds"
-$topmedcmd -persist mark $bamid $markverb completed
-echo `date` $me $SLURM_JOB_ID ok $etime secs >> $console/$bamid.jobids
+SetDB $bamid 'state_gce38bcf_push' 20
+SetDB $bamid 'state_gce38bcf_pull' 20
+#markverb=$me
+SetDB $bamid 'state_gce38bcf' 20
+
+Successful
+Log $etime
 exit
