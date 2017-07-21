f23000b652dd1472ad2d25ae3a4ed6bd11486336
Terry Gliedt <tpg@hps.com>
2017-07-21 16:50:48 -0400

    Simplify when we need to correct permissions for new runs

--- bin/topmedforcew.sh
+++ bin/topmedforcew.sh
@@ -3,28 +3,22 @@
 #   topmedforcew.sh dir
 #
 #	Force in incoming directory to be writable so the monitor can deal with it
-
-#   We only play with files in /incoming/topmed
-topdir=/incoming/topmed
+#   This depends on a file set by topmednewrun.sh   /tmp/run.forcew
+#
+#   08 2,9,14,19 * * * /usr/cluster/topmed/bin/topmedforcew.sh
 topuser=topmed
-a=`echo $1 | grep $topdir`
-if [ "$a" = "" ]; then
-  echo "$0 $topdir/center/run"
-  echo ""
-  echo "Force directories in $topdir to be writable for user $topuser"
-  exit 1
-fi
+topgrp=topmed
+topdir=/incoming/topmed
 
-cd $1
-if [ "$?" != "0" ]; then
-  echo "$0 - Unable to CD to '$1'"
-  exit 4
-fi
-# echo "Forcing '$1' to be writable for the monitor"
-#   This should not be done cause it messes up ACLs. Well that's what was said
-#   however when the owner is not topmed, I cannot rename files in then
-#   directory, so I will continue to mess up the ACLs
-chown -R topmed .
-chgrp topmed .
-chmod 770 .
-chmod 660 Manifest.txt *.md5 *.cram *.crai *.bam *.bai 2> /dev/null   # Failure is OK
+for f in `ls /tmp/*.forcew 2> /dev/null`; do
+  path=`cat $f`
+  cd $topdir/$path
+  if [ "$?" = "0" ]; then
+    echo "Setting ownership and permissions for $path"
+    chown -R topmed .
+    chgrp topmed .
+    chmod 770 .
+    chmod 660 Manifest.txt *.md5 *.cram *.crai *.bam *.bai 2> /dev/null   # Failure is OK
+  fi
+  rm $f
+done
