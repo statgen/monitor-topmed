8cd089d3ce5f4b323d1c489c87eec553983d9c7b
Terry Gliedt <tpg@hps.com>
2017-09-14 09:51:32 -0400

    Allow this to be restarted, rather than start all over

--- bin/topmed_fix.sh
+++ bin/topmed_fix.sh
@@ -48,17 +48,20 @@ if [ "$rgmap" = "" ]; then
   Fail "Unable to find $rgmap"
 fi
 
-stime=`date +%s`
-/net/fantasia/home/hmkang/tools/apigenome.test/bin/cramore cram-update-rg --ref /net/mario/nodeDataMaster/local/ref/gotcloud.ref/hg38/hs38DH.fa --rg-map $rgmap --sam $recab --out $newrecab
-rc=$?
-etime=`date +%s`
-etime=`expr $etime - $stime`
-echo "Corrected recab completed in $etime seconds"
-if [ "$rc" != "0" ]; then
-  echo "Corrected recab for $bamid build $build failed" >> $fixlog
-  Fail "Corrected recab failed" 
+if [ ! -f $newrecab ]; then
+  stime=`date +%s`
+  /net/fantasia/home/hmkang/tools/apigenome.test/bin/cramore cram-update-rg --ref /net/mario/nodeDataMaster/local/ref/gotcloud.ref/hg38/hs38DH.fa --rg-map $rgmap --sam $recab --out $newrecab
+  rc=$?
+  etime=`date +%s`
+  etime=`expr $etime - $stime`
+  echo "Corrected recab completed in $etime seconds"
+  if [ "$rc" != "0" ]; then
+    echo "Corrected recab for $bamid build $build failed" >> $fixlog
+    Fail "Corrected recab failed" 
+  fi
+else
+  echo "Using existing recab $newcab"
 fi
-
 #   Get flagstat values for new file, must match original flagstat
 echo "Calculating flagstat for $newrecab"
 b=`basename $newrecab`
@@ -74,31 +77,25 @@ if [ "$newflagstat" = "" ]; then
   Fail "Unable to get reads of paired in sequencing from '$output' for '$newrecab'"
 fi
 
-origflagstat=`GetDB $bamid b38flagstat`
+origflagstat=`GetDB $bamid cramflagstat`
 if [ "$origflagstat" != "$newflagstat" ]; then
   Fail "New remapped file flagstat incorrect $origflagstat != $newflagstat"
 fi
 echo "New remapped file is valid, flagstats match ($newflagstat)"
 
 #  Create a new index for the file
-$topmedmakeindex $newrecab $build $console/$bamid-$me.out
-if [ "$?" != "0" ]; then
-  Fail "Unable to create index file for '$recab'"
-fi
+CreateIndex $bamid $newrecab
 
-#  Calculate the new MD% for this new file
+#  Calculate the new MD5 for this new file
 echo "Calculating MD5 for $newrecab"
 stime=`date +%s`
-md5=(`md5sum $newrecab`)
-b38cramchecksum=${md5[0]}
-if [ "$b38cramchecksum" = "" ]; then
-  Fail "MD5sum for recab failed"
-fi
+b38cramchecksum=`CalcMD5 $bamid $newrecab`
 etime=`date +%s`
 etime=`expr $etime - $stime`
 echo "MD5SUM for new recab completed in $etime seconds"
 
 #  All is good, put this fie into production
+echo "Replacing $recab with $newrecab"
 mv $newrecab $recab
 if [ "$?" != "0" ]; then
   echo "Unable to move remapped recab for $bamid build $b failed" >> $fixlog
@@ -111,11 +108,12 @@ if [ "$?" != "0" ]; then
 fi
 
 #  Update database
+echo "Updating database values"
 SetDB $bamid b38cramchecksum $b38cramchecksum
 SetDB $bamid state_gce38copy 1
 SetDB $bamid b38flagstat $newflagstat
 
-echo "Corrected recab for $bamid successful" >> $fixlog
+echo "Corrected recab for $bamid $nwdid successful" >> $fixlog
 Successful
 Log $etime
 exit
@@ -167,9 +165,7 @@ if [ "$rc" != "0" ]; then
   echo "MD5sum for $bamid build $b failed" >> $console/fix.log
   Fail "MD5sum failed" 
 fi
-
 SetDB $bamid ${b}cramchecksum $md5
-
 echo "MD5sum for $bamid build $b successful" >> $console/fix.log
 Successful
 Log $etime
@@ -228,7 +224,7 @@ fi
 
 SetDB $bamid ${b}cramchecksum $md5
 
-echo "MD5sum for $bamid build $b successful" >> $console/fix.log
+echo "`date   MD5sum for $bamid build $b successful" >> $console/fix.log
 Successful
 Log $etime
 exit
