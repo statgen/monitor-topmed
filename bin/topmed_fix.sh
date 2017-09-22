d1f78dcb148271ca1b87f751a78d2ff9a01c9471
Terry Gliedt <tpg@hps.com>
2017-09-22 10:06:14 -0400

    New script to rewrite RGMAP headers - remove from fix

--- bin/topmed_fix.sh
+++ bin/topmed_fix.sh
@@ -29,92 +29,22 @@ if [ "$1" = "" ]; then
 fi
 
 bamid=$1
-build=b38
 
 Started
 
 echo "# $0 $bamid $nwdid  -- redo header for mis-mapped b38 crams"
-recab=`$topmedpath wherefile $bamid b38`
-tmpdir=/tmp
 fixlog=$console/fix.log
-
-#  Create a corrected cram
-b=`basename $recab`
-newrecab=$tmpdir/$b
-nwdid=`GetDB $bamid expt_sampleid`
-
-rgmap=`ls /net/topmed8/working/call_sets/rehdr/rgmaps/*/$nwdid.rgmap`
-if [ "$rgmap" = "" ]; then
-  Fail "Unable to find $rgmap"
-fi
-
-if [ ! -f $newrecab ]; then
-  stime=`date +%s`
-  /net/fantasia/home/hmkang/tools/apigenome.test/bin/cramore cram-update-rg --ref /net/mario/nodeDataMaster/local/ref/gotcloud.ref/hg38/hs38DH.fa --rg-map $rgmap --sam $recab --out $newrecab
-  rc=$?
-  etime=`date +%s`
-  etime=`expr $etime - $stime`
-  echo "Corrected recab completed in $etime seconds"
-  if [ "$rc" != "0" ]; then
-    echo "Corrected recab for $bamid build $build failed" >> $fixlog
-    Fail "Corrected recab failed" 
-  fi
-else
-  echo "Using existing recab $newcab"
-fi
-#   Get flagstat values for new file, must match original flagstat
-echo "Calculating flagstat for $newrecab"
-b=`basename $newrecab`
-outfile=/tmp/$b.flagstat
-$samtools flagstat  $newrecab > $outfile
-if [ "$?" != "0" ]; then
-  Fail "Unable to calculate flagstat for $newrecab"
-fi
-a=(`grep 'paired in sequencing' $outfile`)   #   Get paired reads from flagstat
-newflagstat=${a[0]}
-rm -f $outfile
-if [ "$newflagstat" = "" ]; then
-  Fail "Unable to get reads of paired in sequencing from '$output' for '$newrecab'"
-fi
-
-origflagstat=`GetDB $bamid cramflagstat`
-if [ "$origflagstat" != "$newflagstat" ]; then
-  Fail "New remapped file flagstat incorrect $origflagstat != $newflagstat"
-fi
-echo "New remapped file is valid, flagstats match ($newflagstat)"
-
-#  Create a new index for the file
-CreateIndex $bamid $newrecab
-
-#  Calculate the new MD5 for this new file
-echo "Calculating MD5 for $newrecab"
 stime=`date +%s`
-b38cramchecksum=`CalcMD5 $bamid $newrecab`
+/usr/cluster/topmed/bin/topmed_rgmap.sh $bamid $markverb
+if [ "$?" != "0" ]; then
+  echo "Unable to correct rgmap bamid=$bamid" >> $fixlog
+  Fail "Unable to correct rgmap" 
+fi
+
+echo "Corrected rgmap for recab for $bamid" >> $fixlog
+Successful
 etime=`date +%s`
 etime=`expr $etime - $stime`
-echo "MD5SUM for new recab completed in $etime seconds"
-
-#  All is good, put this fie into production
-echo "Replacing $recab with $newrecab"
-mv $newrecab $recab
-if [ "$?" != "0" ]; then
-  echo "Unable to move remapped recab for $bamid build $b failed" >> $fixlog
-  Fail "Unable to move remapped recab failed" 
-fi
-mv $newrecab.crai $recab.crai
-if [ "$?" != "0" ]; then
-  echo "Unable to move remapped crai for $bamid build $b failed" >> $fixlog
-  Fail "Unable to move remapped craiß failed" 
-fi
-
-#  Update database
-echo "Updating database values"
-SetDB $bamid b38cramchecksum $b38cramchecksum
-SetDB $bamid state_gce38copy 1
-SetDB $bamid b38flagstat $newflagstat
-
-echo "Corrected recab for $bamid $nwdid successful" >> $fixlog
-Successful
 Log $etime
 exit
 
---
+++ bin/topmed_rgmap.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+#
+#   topmed_rgmmap.sh bamid|nwdid [markverb]
+#
+#	RG headers in remapped cram are wrong, rebuild fixed cram
+#
+. /usr/cluster/topmed/bin/topmed_actions.inc
+me=rgmap
+markverb=''                 # Avoids setting database unless provided by
+
+if [ "$1" = "" ]; then
+  me=`basename $0`
+  echo "Usage: $me bamid|nwdid  [markverb]"
+  echo ""
+  echo "RG headers in remapped cram are wrong, rebuild fixed cram."
+  exit 1
+fi
+
+bamid=$1
+if [ "$2" != "" ]; then
+  markverb=$2               # Allow database state for an action to be set
+fi
+build=b38
+
+Started
+
+echo "# $0 $bamid $nwdid  -- redo header for mis-mapped b38 crams"
+recab=`$topmedpath wherefile $bamid b38`
+tmpdir=/tmp
+
+#  Create a corrected cram
+b=`basename $recab`
+newrecab=$tmpdir/$b
+nwdid=`GetDB $bamid expt_sampleid`
+
+#   Build correct rg mapping file
+rgmap=$tmpdir/$nwdid.rgmap
+/usr/cluster/topmed/bin/topmedrgmap.pl $bamid $rgmap
+if [ "$?" != "0" ]; then
+  Fail "Unable to create $rgmap"
+fi
+
+if [ ! -f $newrecab ]; then
+  stime=`date +%s`
+  /net/fantasia/home/hmkang/tools/apigenome.test/bin/cramore cram-update-rg --ref /net/mario/nodeDataMaster/local/ref/gotcloud.ref/hg38/hs38DH.fa --rg-map $rgmap --sam $recab --out $newrecab
+  rc=$?
+  rm -f $rgmap
+  etime=`date +%s`
+  etime=`expr $etime - $stime`
+  echo "Corrected recab completed in $etime seconds"
+  if [ "$rc" != "0" ]; then
+    Fail "Corrected recab failed" 
+  fi
+else
+  echo "Using existing recab $newcab"
+fi
+#   Get flagstat values for new file, must match original flagstat
+newflagstat=`CalcFlagstat $bamid $newrecab`
+if [ "$newflagstat" = "" ]; then
+  Fail "Unable to get reads of paired in sequencing for '$newrecab'"
+fi
+
+origflagstat=`GetDB $bamid cramflagstat`
+if [ "$origflagstat" != "$newflagstat" ]; then
+  Fail "New remapped file flagstat incorrect $origflagstat != $newflagstat"
+fi
+echo "New remapped file is valid, flagstats match ($newflagstat)"
+
+#  Create a new index for the file
+CreateIndex $bamid $newrecab
+
+#  Calculate the new MD5 for this new file
+echo "Calculating MD5 for $newrecab"
+stime=`date +%s`
+b38cramchecksum=`CalcMD5 $bamid $newrecab`
+etime=`date +%s`
+etime=`expr $etime - $stime`
+echo "MD5SUM for new recab completed in $etime seconds"
+
+#  All is good, put this file into production
+echo "Replacing $recab with $newrecab"
+mv $newrecab $recab
+if [ "$?" != "0" ]; then
+  Fail "Unable to move remapped recab failed" 
+fi
+mv $newrecab.crai $recab.crai
+if [ "$?" != "0" ]; then
+  Fail "Unable to move remapped craiß failed" 
+fi
+
+#  Update database
+echo "Updating database values"
+SetDB $bamid b38cramchecksum $b38cramchecksum
+SetDB $bamid state_gce38copy 1
+SetDB $bamid b38flagstat $newflagstat
+
+echo "Corrected recab for $bamid $nwdid was successful"
+Successful
+exit
