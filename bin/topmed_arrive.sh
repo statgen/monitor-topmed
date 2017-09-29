7a0c48f7c53abe5bcb4b7cea82e6291678a0530a
Terry Gliedt <tpg@hps.com>
2017-09-29 10:10:31 -0400

    Use GetBuild

--- bin/topmed_arrive.sh
+++ bin/topmed_arrive.sh
@@ -32,23 +32,11 @@ Started
 #nwdid=`GetNWDID $bamid`            # Only case where we do not get NWDID right away
 
 #   Determine build used to generate this bam/cram
-build=''
-l=(`$samtools view -H $bamfile | grep '@SQ' | cut -f 1-3 2>/dev/null | head -2`)
-if [ "${l[2]}" = "LN:249250621" -a "${l[5]}" = "LN:243199373" ]; then
+c=`$topmedcmd show $bamid center`
+if [ "$c" = "illumina" ]; then      # Hardcode build for this center
   build=37
 else
-  if [ "${l[2]}" = "LN:248956422" -a "${l[5]}" = "LN:242193529" ]; then
-    build=38
-  else
-    c=`$topmedcmd show $bamid center`
-    if [ "$c" = "illumina" ]; then       # Hardcode build for this center
-      build=37
-    fi
-  fi
-fi
-if [ "$build" = "" ]; then
-  Fail "Unable to determine build for '$bamfile' (L2=${l[2]} L5=${l[5]}"
-  exit 4
+  build=`GetBuild $bamid $bamfile`
 fi
 echo "File '$bamfile' [$bamid] is from build $build"
 SetDB $bamid 'build' $build
