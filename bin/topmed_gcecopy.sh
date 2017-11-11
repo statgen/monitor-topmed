f003878ca80b6b828959173c31cb0145b9a56e7e
Terry Gliedt <tpg@hps.com>
2017-11-11 06:29:18 -0500

    Simplify code, more like awscopy

--- bin/topmed_gcecopy.sh
+++ bin/topmed_gcecopy.sh
@@ -10,6 +10,26 @@ me=gcecopy
 markverb=$me
 stat="stat --printf=%s --dereference"
 
+#------------------------------------------------------------------
+# Subroutine:
+#   Copy2GCE(f, gcefile)
+#   Copy file to GCE as gcefile
+#------------------------------------------------------------------
+function Copy2GCE {
+  local f=$1
+  local gcefile=$2
+  if [ "${f##*.}" = "cram" ]; then
+    gs=$gsutilbig
+  else
+    gs=$gsutil
+  fi
+  echo "====> Copying to $gcefile"
+  $gs cp $f $copyuri/$gcefile       # Someday we will upgrade and -q will work
+  if [ "$?" != "0" ]; then
+    Fail "Failed to copy $f to GCE as $gcefile"
+  fi
+}
+
 if [ "$1" = "-submit" ]; then
   shift
   bamid=`$topmedcmd show $1 bamid`
@@ -40,12 +60,12 @@ fi
 if [ ! -f $recabcram ]; then
   Fail "CRAM file for '$bamid' does not exist: $recabcram"
 fi
-baserecabcram=`basename $recabcram`
 sizerecabcram=`$stat $recabcram`
-recabcrai=$recabcram.crai
 
 #   Create the index file as necessary
 CreateIndex $bamid $recabcram
+recabcrai=$recabcram.crai
+sizerecabcrai=`$stat $recabcrai`
 
 #   Get checksum for b38 file or calculate it
 b38cramchecksum=`GetDB $bamid b38cramchecksum`
@@ -58,134 +78,87 @@ if [ "$b38cramchecksum" = "" ]; then
   echo "MD5SUM for recab completed in $etime seconds"
   SetDB $bamid b38cramchecksum $b38cramchecksum
 fi
-f=`basename $recabcram`
-echo "$b38cramchecksum $f" > $recabcram.md5
-
-#   Pass along flagstat too
-flagstat=`GetDB $bamid cramflagstat`
-echo "$flagstat + 0 paired in sequencing" > $recabcram.flagstat
-
-baserecabcrai=`basename $recabcrai`
-sizerecabcrai=`$stat $recabcrai`
 
 #   Get bcf file for remapped cram file
-recabbcf=`$topmedpath wherefile $bamid bcf`
-if [ "$recabbcf" = "" ]; then
-  Fail "Unable to determine BCF file for '$bamid'"
-fi
-if [ ! -f $recabbcf ]; then
-  Fail "BCF file for '$bamid' does not exist: $recabbcf"
-fi
-baserecabbcf=`basename $recabbcf`
-sizerecabbcf=`$stat $recabbcf`
+#recabbcf=`$topmedpath wherefile $bamid bcf`
+#if [ "$recabbcf" = "" ]; then
+#  Fail "Unable to determine BCF file for '$bamid'"
+#fi
+#if [ ! -f $recabbcf ]; then
+#  Fail "BCF file for '$bamid' does not exist: $recabbcf"
+#fi
+#sizerecabbcf=`$stat $recabbcf`
 
-recabcsi=$recabbcf.csi
-if [ ! -f $recabcsi ]; then
-  echo "CSI missing, trying to create it"
-  $bcftools index $recabbcf
-  if [ "$?" != "0" ]; then
-    Fail "Unable to create CSI for $bamid' for: $recabbcf"
-  fi
-fi
-if [ ! -f $recabcsi ]; then
-  Fail "CSI file for '$bamid' does not exist: $recabcsi"
-fi
-baserecabcsi=`basename $recabcsi`
-sizerecabcsi=`$stat $recabcsi`
+#recabcsi=$recabbcf.csi
+#if [ ! -f $recabcsi ]; then
+#  echo "CSI missing, trying to create it"
+#  $bcftools index $recabbcf
+#  if [ "$?" != "0" ]; then
+#    Fail "Unable to create CSI for $bamid' for: $recabbcf"
+#  fi
+#fi
+#if [ ! -f $recabcsi ]; then
+#  Fail "CSI file for '$bamid' does not exist: $recabcsi"
+#fi
+#sizerecabcsi=`$stat $recabcsi`
 
 #======================================================================
 #   All the files of interest exist locally
 #   Copy the file to the remote destination only if the size differs
-#   Just to be sure if the cram or bcf is missing, copy the index too
 #======================================================================
-replacelist=''
-rlist=''
-cmissinglist=''
-cmissing=''
 copyuri=`$topmedpath wherepath $nwdid gceupload`
 
+#   Now get the sizes of files of interest in AWS
+baserecabcram=`basename $recabcram`
 l=(`$gsutil stat $copyuri/$baserecabcram | grep Content-Length:`)
-gcesize=${l[1]}
-if [ "$sizerecabcram" != "$gcesize" ]; then
-  echo "$baserecabcram sizes= $sizerecabcram / $gcesize"
-  if [ "$gcesize" = "" ]; then
-    cmissing="$cmissing $baserecabcram $baserecabcrai $recabcram.md5 $recabcram.flagstat"
-    cmissinglist="$cmissinglist $recabcram $recabcrai $recabcram.md5 $recabcram.flagstat"
-  else
-    replacelist="$replacelist $recabcram $recabcrai $recabcram.md5 $recabcram.flagstat"
-    rlist="$rlist $baserecabcram $baserecabcrai  $recabcram.md5 $recabcram.flagstat"
-  fi
+gcesizerecabcram=${l[1]}
+
+baserecabcrai=$baserecabcram.crai
+l=(`$gsutil stat $copyuri/$baserecabcrai | grep Content-Length:`)
+gcesizerecabcrai=${l[1]}
+
+#baserecabbcf=`basename $recabbcf`
+#l=(`$gsutil stat $copyuri/$baserecabbcf | grep Content-Length:`)
+#gcesizerecabbcf=${l[1]}
+
+#baserecabcsi=$baserecabbcf.csi
+#l=(`$gsutil stat $copyuri/$baserecabcsi | grep Content-Length:`)
+#gcesizerecabcsi=${l[1]}
+
+#   Force everthing to be sent, usually for testing
+#gcesizerecabcram=`expr $gcesizerecabcram - 1`
+#gcesizerecabcrai=`expr $gcesizerecabcrai - 1`
+#gcesizerecabbcf=`expr $gcesizerecabbcf - 1`
+#gcesizerecabcsi=`expr $gcesizerecabcsi - 1`
+
+#   Send files that have changed size
+if [ "$sizerecabcram" != "$gcesizerecabcram" ]; then
+  tmpf=/run/shm/$$
+  Copy2GCE $recabcram $baserecabcram
+  echo "$b38cramchecksum $baserecabcram" > $tmpf
+  Copy2GCE $tmpf $baserecabcram.md5
+  flagstat=`GetDB $bamid b38flagstat`
+  echo "$flagstat + 0 paired in sequencing" > $tmpf
+  Copy2GCE $tmpf $baserecabcram.flagstat
+  rm -f $tmpf
 else
-  l=(`$gsutil stat $copyuri/$baserecabcrai | grep Content-Length:`)
-gcesize=${l[1]}
-  if [ "$sizerecabcrai" != "$gcesize" ]; then
-    echo "$baserecabcrai sizes= $sizerecabcrai / $gcesize"
-    if [ "$gcesize" = "" ]; then
-      cmissing="$cmissing $baserecabcrai $recabcram.md5 $recabcram.flagstat"
-      cmissinglist="$cmissinglist $recabcrai $recabcram.md5 $recabcram.flagstat"
-    else
-      replacelist="$replacelist $recabcrai $recabcram.md5 $recabcram.flagstat"
-      rlist="$rlist $baserecabcrai $recabcram.md5 $recabcram.flagstat"
-    fi
-  fi
+  echo "No need to send unchanged $baserecabcram"
 fi
-
-l=(`$gsutil stat $copyuri/$baserecabbcf | grep Content-Length:`)
-gcesize=${l[1]}
-if [ "$sizerecabbcf" != "$gcesize" ]; then
-  echo ""
-  echo "$baserecabbcf sizes= $sizerecabbcf / $gcesize"
-  if [ "$gcesize" = "" ]; then
-    cmissing="$cmissing $baserecabbcf $baserecabcsi"
-    cmissinglist="$cmissinglist $recabbcf $recabcsi"
-  else
-    replacelist="$replacelist $recabbcf $recabcsi"
-    rlist="$rlist $baserecabbcf $baserecabcsi"
-  fi
+if [ "$sizerecabcrai" != "$gcesizerecabcrai" ]; then
+  Copy2GCE $recabcrai $baserecabcrai
 else
-  l=(`$gsutil stat $copyuri/$baserecabcsi | grep Content-Length:`)
-gcesize=${l[1]}
-  if [ "$sizerecabcsi" != "$gcesize" ]; then
-    echo "$baserecabcsi sizes= $sizerecabcsi / $gcesize"
-    if [ "$gcesize" = "" ]; then
-      cmissing="$cmissing $baserecabcs"
-      cmissinglist="$cmissinglist $recabcsi"
-    else
-      replacelist="$replacelist $recabcsi"
-      rlist="$rlist $baserecabcsi"
-    fi
-  fi
+  echo "No need to send unchanged $baserecabcrai"
 fi
-
-#   First copy just the files that are missing
-if [ "$cmissing" != "" ]; then
-  echo ""
-  echo ""
-  echo "====> Copying MISSING files to $copyuri:  $cmissing"
-  for f in $cmissinglist; do
-    basef=`basename $f`
-    echo "Copying $basef"
-    $gsutilbig cp $f $copyuri/$basef
-    if [ "$?" != "0" ]; then
-      Fail "Failed to copy $basef to GCE"
-    fi
-  done
-fi
-
-#   Now, copy all the files that need replacing
-if [ "$replacelist" != "" ]; then
-  echo ""
-  echo "====> Replace in $copyuri:  $rlist"
-#Fail "Replace in $copyuri:  $rlist"
-  for f in $replacelist; do
-    basef=`basename $f`
-    echo "Copying $basef"
-    $gsutilbig cp $f $copyuri/$basef
-    if [ "$?" != "0" ]; then
-      Fail "Failed to copy $basef to GCE"
-    fi
-  done
-fi
+#if [ "$sizerecabbcf" != "$gcesizerecabbcf" ]; then
+#  Copy2GCE $recabbcf $baserecabbcf
+#else
+#  echo "No need to send unchanged $baserecabbcf"
+#fi
+#if [ "$sizerecabcsi" != "$gcesizerecabcsi" ]; then
+#  Copy2GCE $recabcsi $baserecabcsi
+#else
+#  echo "No need to send unchanged $baserecabcsi"
+#fi
 
 echo "GCE files for $bamid $nwdid are up to date"
 $gsutil ls -l $copyuri
