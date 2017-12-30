4dacc98ec4226bc66a110e5e0316f16c26292d3e
Terry Gliedt <tpg@hps.com>
2017-12-30 07:08:48 -0500

    Yet another attempt to create the backup directory properly

--- bin/topmedforcew.sh
+++ bin/topmedforcew.sh
@@ -59,46 +59,49 @@ function ProcessRuns {
       newrun=/net/topmed/incoming/topmed/$center/$r
       echo -n "$now Monitor knows about ${#runs[@]} runs for $center  "
       # Determine if this is year 2 (bam) or 3 (cram) - determines where the backup dir is
-      year=0
-      if [ -f $r/Manifest.txt ]; then
-        s=`head -1 $r/Manifest.txt grep cram 2> /dev/null`
-        if [ "$s" != "" ]; then year=3; fi
-        s=`head -1 $r/Manifest.txt grep bam 2> /dev/null`
-        if [ "$s" != "" ]; then year=2; fi
-      fi
+      #year=0
+      #if [ -f $r/Manifest.txt ]; then
+      #  s=`head -1 $r/Manifest.txt grep cram 2> /dev/null`
+      #  if [ "$s" != "" ]; then year=3; fi
+      #  s=`head -1 $r/Manifest.txt grep bam 2> /dev/null`
+      #  if [ "$s" != "" ]; then year=2; fi
+      #fi
       #  With luck we have determined if this year 2 or 3
-      if [ "$year" = "0" ]; then
-        echo "$now Cannot figure out the year for '$r'"
-        ls -l $r/Manifest.txt
-        return
-      else
-        date
-        echo "$now New run '$r' year $year for '$center' found"
-        if [ "$myhost" != "topmed" ]; then
-          su $topuser -c "ln -s ../../../../$myhost/incoming/topmed/$center/$r $newrun"
-          echo "$now Set symlink to $newrun"
-        fi
-        #     Sort out backup directory for run
-        if [ "$year" = "2" ]; then
-          backuphost=topmed7          # Year 2 we make a directory elsewhere for backups
-          su $topuser -c "mkdir -p /net/$backuphost/working/backups/incoming/topmed/$center/$r"
-          masterbackuprun="../../../../../../$backuphost/working/backups/incoming/topmed/$center/$r"
-        fi
-        if [ "$year" = "3" ]; then
-          backuphost=$myhost          # Year 3 uses the incoming directory for 'backup'
-          masterbackuprun="../../../../../../$backuphost/incoming/topmed/$center/$r"
-        fi
-        newbackuprun=/net/topmed/working/backups/incoming/topmed/$center/$r
-        su $topuser -c "ln -s $masterbackuprun $newbackuprun"
-        echo "$now BACKUP Symlink set to host '$backuphost' for $newbackuprun ($masterbackuprun)"
-        n=`expr $n + 1`
-        #     Force ownership of run and files since we are root
-        chown -R $topuser $r
-        chgrp -R $topgrp $r
-        chmod 770 $r
-        chmod 660 $r/Manifest.txt $r/*.cram $r/*.bam 2> /dev/null
-        echo "$now Ownership and permissions set for $center/$r"
+      #if [ "$year" = "0" ]; then
+      #  echo "$now Cannot figure out the year for '$r'"
+      #  ls -l $r/Manifest.txt
+      #  continue
+      #fi
+      #   We have a new run, set it up
+      date
+      echo "$now New run '$r' year $year for '$center' found"
+      if [ "$myhost" != "topmed" ]; then
+        su $topuser -c "ln -s ../../../../$myhost/incoming/topmed/$center/$r $newrun"
+        echo "$now Set symlink to $newrun"
+        backuphost=$myhost          # Year 3 uses the incoming directory for 'backup'
+        masterbackuprun="../../../../../../$backuphost/incoming/topmed/$center/$r"
       fi
+      #     Sort out backup directory for run
+      #if [ "$year" = "2" ]; then
+      #  backuphost=topmed7          # Year 2 we make a directory elsewhere for backups
+      #  su $topuser -c "mkdir -p /net/$backuphost/working/backups/incoming/topmed/$center/$r"
+      #  masterbackuprun="../../../../../../$backuphost/working/backups/incoming/topmed/$center/$r"
+      #fi
+      #if [ "$year" = "3" ]; then
+      #  backuphost=$myhost          # Year 3 uses the incoming directory for 'backup'
+      #  masterbackuprun="../../../../../../$backuphost/incoming/topmed/$center/$r"
+      #fi
+
+      newbackuprun=/net/topmed/working/backups/incoming/topmed/$center/$r
+      su $topuser -c "ln -s $masterbackuprun $newbackuprun"
+      echo "$now BACKUP Symlink set to host '$backuphost' for $newbackuprun ($masterbackuprun)"
+      n=`expr $n + 1`
+      #     Force ownership of run and files since we are root
+      chown -R $topuser $r
+      chgrp -R $topgrp $r
+      chmod 770 $r
+      chmod 660 $r/Manifest.txt $r/*.cram $r/*.bam 2> /dev/null
+      echo "$now Ownership and permissions set for $center/$r"
     fi
   done
 
@@ -137,9 +140,10 @@ sleep $wait                     # Wait a little bit to avoid messing up logs
 locallog=/tmp/topmedforcew.log.$$
 ProcessRuns baylor > $locallog
 ProcessRuns washu  >> $locallog
-if [ ! -z $locallog ]; then
+if [ -s $locallog ]; then
   su $topuser -c "(cat $locallog >> /net/topmed/working/topmed-output/topmed_init.log)"
+  chown topmed $locallog          # So I can remove these
+else
+  rm $locallog
 fi
-chown topmed $locallog          # Then I remove these
-#rm -f $locallog
 exit
