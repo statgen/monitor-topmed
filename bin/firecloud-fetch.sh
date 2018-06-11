aa159acc9cf0fa3dc99e71851a1397c869a6a127
Terry Gliedt <tpg@hps.com>
2018-06-11 10:06:38 -0400

    Another attempt to automate fetching fire cloud data

--- bin/firecloud-fetch.sh
+++ bin/firecloud-fetch.sh
@@ -8,13 +8,17 @@
 #        https://portal.firecloud.org/#workspaces/...
 #
 me=`basename $0`
-tmpdir=bucket-incoming
+#   Copy bucket data into here
+ddndir=/net/ddn/incoming/topmed/broad
+#   After we think DDN works, we download directly into DDN
+tmpdir=$ddndir
+tmpdir=/net/topmed9/incoming/topmed/year4-tempdata
 
 if [ "$2" = "" ]; then
   echo "$me - Fetch data from the BROAD from firecloud"
   echo ""
   echo "Usage: $me newname url"
-  echo "  newname - name of directory where incoming data should go"
+  echo "  newname - name of directory where DDN incoming data should go"
   echo "  url - firecloud URL provided by Broad"
   echo ""
   exit 1
@@ -41,10 +45,12 @@ else
   echo "Created directory '$dir'"
 fi
 
-#   Data goes into $dir eventually, but we really download to incoming initially
-mkdir $dir/$tmpdir 2> /dev/null
+#   Data goes into $ddndir/$dir   For now download to $tmpdir/$dir
+cd $ddndir || exit 4
+mkdir -p $dir
+cd $tmpdir || exit 4            # Someday remove these two lines
+mkdir -p $dir
 cd $dir || exit 4
-cd $tmpdir || exit 4
 
 #   Get bucket name
 token=`gcloud auth print-access-token`
@@ -54,8 +60,28 @@ if [ "$token" = "" ]; then
 fi
 echo ""
 
-tmpf=curldata
-curl -s -X GET -H "Authorization:Bearer $token " --header 'Accept: text/html' "$url" > $tmpf
+#   We can't trust broad to send us exactly the same URL format
+#   so here we try to construct the URL we really need
+#   We want: https://api.firecloud.org/api/workspaces/broad-genomics-data/TOPMED_etc
+#   but maybe we get: https://portal.firecloud.org/#workspaces/broad-genomics-data/TOPMED_etc
+x=`echo $url | grep 'https://portal.firecloud.org/'`
+if [ "$x" != "" ]; then
+  echo "Converting URL into something that will work for us"
+  x=`echo $url | tr -d \# | sed -e 's+https://portal.firecloud.org/++'`
+  if [ "$x" = "" ]; then
+    echo "Failed to construct URL like: https://api.firecloud.org/api/workspaces/broad-genomics-data/.."
+    exit 3
+  fi
+  url="https://api.firecloud.org/api/$x"
+  echo "We will use URL=$url"
+fi
+
+tmpf=firecloud.log.txt
+date > $tmpf
+echo $url >> $tmpf
+echo "" >> $tmpf
+curl -s -X GET -H "Authorization:Bearer $token " --header 'Accept: text/html' "$url" >> $tmpf
+echo "" >> $tmpf
 
 cat > $tmpf.pl <<EOF
 \$a=\`cat $tmpf\`;
@@ -63,45 +89,72 @@ if (\$a =~ /.bucketName.:.([^\"]+)/) { print \$1; }
 EOF
 
 bucket=`perl $tmpf.pl`
-if [ "$uri" = "" ]; then
-  echo "$me - Unable to find buckName from the URL you provided"
-  echo "See curl output in $tmpf"
+if [ "$bucket" = "" ]; then
+  echo "$me - Unable to find buckName from URL=$url" | tee -a $tmpf
+  echo "See curl output in `pwd`/$tmpf" | tee -a $tmpf
   exit 2
 fi
-uri="gs://$uri"
+uri="gs://$bucket"
+echo $uri >> $tmpf
 
 #   Finally know what we are supposed to do
-echo "Fetching data from bucket $uri"
-gsutil -m rsync $uri .
+stime=`date +%s`
+echo ""
+echo "Fetching bucket $uri to `pwd`"
+echo "This will take quite some time"
+echo ""
+gsutil -m rsync -r $uri .
 if [ "$?" != "0" ]; then
-  echo "$me -FAILED to fetch all the data from $uri"
+  echo "$me -FAILED to fetch all the data from $uri" | tee -a $tmpf
   exit 6
 fi
+etime=`date +%s`
+etime=`expr $etime - $stime`
+echo "Fetch of CRAMS completed in $etime seconds" | tee -a $tmpf
 
-echo "Now merging files of interest into $dir"
-cd $dir || exit 4
-for x in cram crai md5; do
-  echo -n "$x "
-  find $tmpdir -name \*.$x -exec mv {} . \;
-done
+#   Must now get MD5 for each cram file
+stime=`date +%s`
 echo ""
+echo "Now get the MD5s for CRAM files"
+for cram in `find . -name \*.cram -print`; do
+  c=`echo $cram | sed -e 's:./::'`
+  gsutil hash -h $uri/$c | grep md5 > $c.md5
+done
+etime=`date +%s`
+etime=`expr $etime - $stime`
+echo "Fetch of MD5s completed in $etime seconds" | tee -a $tmpf
+
+#   Files are in holding directory - copy the stuff we want to DDN
+#   Must also get MD5 for the cram file
+#   Someday we copy files directly from the bucket to DDN.
+stime=`date +%s`
+echo ""
+echo "Now copying files of interest into $ddndir/$dir"
+for f in `find . -name \*.cram -print`; do
+    nwd=${f%.*}
+    rsync -a ${nwd}.* $ddndir/$dir 
+done
+etime=`date +%s`
+etime=`expr $etime - $stime`
+echo "Copy of CRAMS to DDN completed in $etime seconds" | tee -a $tmpf
+cp -p $tmpf $ddndir/$dir
+
+#   Clean out cruft from the Broad. For now the cruft is left in $tmpdir/$dir
+cd $ddndir/$dir || exit 8
 n=`ls *.cram|wc -l`
 if [ "$n" = "0" ]; then
   echo "$me - something went wrong, no CRAMS found"
   exit 9
 fi
-echo "Cleaning out detritus from the bucket"
-rm -rf $tmpdir
-echo "SUCCESSFUL  Fetched $n $uri files to $dir"
+echo ""
+echo "SUCCESSFUL  Fetched $n $uri files to $ddndir/$dir" | tee -a $tmpf
 
-echo "Creating Manifest.txt from the MD5 files"
+echo "Creating Manifest.txt from the MD5 files" | tee -a $tmpf
 mf=Manifest.txt
 rm -f $mf
 for f in *.cram; do
-  md5=`cat $f.md5`
-  echo "$md5   $f" >> $mf
+  md5=(`cat $f.md5`)
+  echo "${md5[2]}   $f" >> $mf
 done
+echo "Created in $ddndir/$dir/$mf" | tee -a $tmpf
 ls -l $mf
-
-  
-
