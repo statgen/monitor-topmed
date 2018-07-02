5c341d93237dbde28cabaaa1f6de488d09181439
Terry Gliedt <tpg@hps.com>
2018-07-02 11:03:27 -0400

    Clean up so script is not interactive and does no extra backups

--- bin/firecloud-fetch.sh
+++ bin/firecloud-fetch.sh
@@ -2,159 +2,168 @@
 #
 #   Fetch data from the BROAD from firecloud
 #
-#   Usage: cd where you want data copied
-#       firecloud-fetch.sh URL
+#   Usage:  firecloud-fetch.sh  URL  localdir
+#
 #   where URL was sent by Broad and probably looks like:
-#        https://portal.firecloud.org/#workspaces/...
+#         https://portal.firecloud.org/#workspaces/...
+#   and   localdir  is either an intended directory name on 
+#         /net/ddn/incoming/topmed/broad or else an absolute 
+#         pathname to somewhere else
+#   Not interactive, so this can be run in the background
 #
 me=`basename $0`
-#   Copy bucket data into here
-ddndir=/net/ddn/incoming/topmed/broad
-#   After we think DDN works, we download directly into DDN
-tmpdir=$ddndir
-tmpdir=/net/topmed9/incoming/topmed/year4-tempdata
+convert_checksums=/net/topmed/incoming/study.reference/study.reference/nhlbi.4275.gsutil.checksums.awk
 
-if [ "$2" = "" ]; then
+if [ "$#" -lt 2 ]; then
   echo "$me - Fetch data from the BROAD from firecloud"
   echo ""
-  echo "Usage: $me newname url"
-  echo "  newname - name of directory where DDN incoming data should go"
+  echo "Usage:  $me  url  localdir"
   echo "  url - firecloud URL provided by Broad"
+  echo "  localdir - name of directory where DDN incoming data should go,"
+  echo "  either a relative path in  /net/ddn/incoming/topmed/broad"
+  echo "  or an absolute pathname for somewhere else"
   echo ""
   exit 1
 fi
-dir=$1
-url=$2
 
-echo -n "Do you really want to fetch bucket data to '$dir'? (y/n) "
-read a
-if [ "$a" != "y" ]; then
-  echo "Nothing done"
-  exit 3
-fi
+url=$1
+localdir=$2
+#   Copy bucket data into here
+ddndir=/net/ddn/incoming/topmed/broad
 
-if [ -d $dir ]; then
-  echo -n "Directory '$dir' already exists - continue? (y/n) "
-  read a
-  if [ "$a" != "y" ]; then
-    echo "Nothing done"
-    exit 3
-  fi
+abs=`echo $localdir | cut -c 1`
+if [ "$abs" != "/" ]; then
+  dir=${ddndir}/$localdir
 else
-  mkdir $dir || exit 4
-  echo "Created directory '$dir'"
+  dir=$localdir
 fi
-
-#   Data goes into $ddndir/$dir   For now download to $tmpdir/$dir
-cd $ddndir || exit 4
-mkdir -p $dir
-cd $tmpdir || exit 4            # Someday remove these two lines
 mkdir -p $dir
 cd $dir || exit 4
+
+logfile=fetch.log
+echo ''   >  $logfile
+echo "`date`	`uname -n`" >> $logfile
+echo $me  >> $logfile
+echo $url >> $logfile
+echo $localdir >> $logfile
+echo '' >> $logfile
 
 #   Get bucket name
 token=`gcloud auth print-access-token`
 if [ "$token" = "" ]; then
-  echo "Unable to get gcloud token with 'gcloud auth print-access-token'"
+  echo "Unable to get gcloud token with 'gcloud auth print-access-token'" >> $logfile
   exit 2
 fi
-echo ""
 
 #   We can't trust broad to send us exactly the same URL format
 #   so here we try to construct the URL we really need
-#   We want: https://api.firecloud.org/api/workspaces/broad-genomics-data/TOPMED_etc
-#   but maybe we get: https://portal.firecloud.org/#workspaces/broad-genomics-data/TOPMED_etc
+#   We want: https://api.firecloud.org/api/workspaces/broad-genomics-data/Gabriel_TOPMED_etc
+#   but maybe we get: https://portal.firecloud.org/#workspaces/broad-genomics-data/Gabriel_TOPMED_etc
+
 x=`echo $url | grep 'https://portal.firecloud.org/'`
 if [ "$x" != "" ]; then
-  echo "Converting URL into something that will work for us"
+  echo "Converting URL into something that will work for us" >> $logfile
   x=`echo $url | tr -d \# | sed -e 's+https://portal.firecloud.org/++'`
   if [ "$x" = "" ]; then
-    echo "Failed to construct URL like: https://api.firecloud.org/api/workspaces/broad-genomics-data/.."
+    echo "Failed to construct URL like: https://api.firecloud.org/api/workspaces/broad-genomics-data/.." >> $logfile
     exit 3
   fi
   url="https://api.firecloud.org/api/$x"
-  echo "We will use URL=$url"
+  echo "We will use URL=$url" >> $logfile
 fi
+echo '' >> $logfile
+echo $url >> $logfile
+echo '' >> $logfile
+curl -s -X GET -H "Authorization:Bearer $token " --header 'Accept: text/html' "$url" >> $logfile
+echo '' >> $logfile
+echo '' >> $logfile
 
-tmpf=firecloud.log.txt
-date > $tmpf
-echo $url >> $tmpf
-echo "" >> $tmpf
-curl -s -X GET -H "Authorization:Bearer $token " --header 'Accept: text/html' "$url" >> $tmpf
-echo "" >> $tmpf
+#   Build a perl script which picks the actual Google bucket name 
+#   out of the reply from curl that's already in the logfile.
 
-cat > $tmpf.pl <<EOF
-\$a=\`cat $tmpf\`;
+cat > extract.pl <<EOF
+\$a=\`cat $logfile\`;
 if (\$a =~ /.bucketName.:.([^\"]+)/) { print \$1; }
 EOF
 
-bucket=`perl $tmpf.pl`
+bucket=`perl extract.pl`
 if [ "$bucket" = "" ]; then
-  echo "$me - Unable to find buckName from URL=$url" | tee -a $tmpf
-  echo "See curl output in `pwd`/$tmpf" | tee -a $tmpf
+  echo "$me - Unable to find bucketName from URL=$url" >> $logfile
+  echo "See curl output in `pwd`/$logfile" >> $logfile
   exit 2
 fi
 uri="gs://$bucket"
-echo $uri >> $tmpf
+echo $uri   >> $logfile
+echo  ''    >> $logfile
+echo "`date`\t \tBuild Manifest.txt" >> $logfile
+
+#   Get Google data to build the file Manifest.txt
+#   Checksums are space-separated, but 'cut -f' looks for tab.
+
+gsutil ls -r -l ${uri}/'**' > gsutil.file.list
+gsutil ls -r -L ${uri}/'**' > gsutil.complete
+awk -f $convert_checksums  gsutil.complete > gsutil.checksums
+grep -v cram.crai gsutil.checksums | cut -f 1 > Manifest.txt
+echo `date` >> $logfile
 
 #   Finally know what we are supposed to do
+
+mkdir incoming
+cd    incoming || exit 5
+
 stime=`date +%s`
-echo ""
-echo "Fetching bucket $uri to `pwd`"
-echo "This will take quite some time"
-echo ""
-gsutil -m rsync -r $uri .
+echo '' >> ../$logfile
+echo "Fetching bucket $uri to `pwd`"  >> ../$logfile
+echo "This will take quite some time" >> ../$logfile
+echo '' >> ../$logfile
+gsutil -m -q rsync -r $uri . >> ../$logfile 2>&1
 if [ "$?" != "0" ]; then
-  echo "$me -FAILED to fetch all the data from $uri" | tee -a $tmpf
+  echo "$me -FAILED to fetch all the data from $uri" >> ../$logfile
   exit 6
 fi
 etime=`date +%s`
 etime=`expr $etime - $stime`
-echo "Fetch of CRAMS completed in $etime seconds" | tee -a $tmpf
+echo "Fetch of CRAMS completed in $etime seconds" >> ../$logfile
 
-#   Must now get MD5 for each cram file
-stime=`date +%s`
-echo ""
-echo "Now get the MD5s for CRAM files"
-for cram in `find . -name \*.cram -print`; do
-  c=`echo $cram | sed -e 's:./::'`
-  gsutil hash -h $uri/$c | grep md5 > $c.md5
-done
-etime=`date +%s`
-etime=`expr $etime - $stime`
-echo "Fetch of MD5s completed in $etime seconds" | tee -a $tmpf
+cd ..
 
-#   Files are in holding directory - copy the stuff we want to DDN
-#   Must also get MD5 for the cram file
-#   Someday we copy files directly from the bucket to DDN.
+#   Files are in holding directory 'incoming' - copy the stuff we want to $dir
+#   This script already copies files directly from the bucket to DDN.
 stime=`date +%s`
-echo ""
-echo "Now copying files of interest into $ddndir/$dir"
+echo `date` >> $logfile
+echo "Now copying files of interest into $dir" >> $logfile
 for f in `find . -name \*.cram -print`; do
     nwd=${f%.*}
-    rsync -a ${nwd}.* $ddndir/$dir 
+    mv ${nwd}.* .
 done
 etime=`date +%s`
 etime=`expr $etime - $stime`
-echo "Copy of CRAMS to DDN completed in $etime seconds" | tee -a $tmpf
-cp -p $tmpf $ddndir/$dir
-
-#   Clean out cruft from the Broad. For now the cruft is left in $tmpdir/$dir
-cd $ddndir/$dir || exit 8
+echo "Copy of CRAMS to DDN completed in $etime seconds" >> $logfile
+echo `date` >> $logfile
 n=`ls *.cram|wc -l`
 if [ "$n" = "0" ]; then
-  echo "$me - something went wrong, no CRAMS found"
+  echo "$me - something went wrong, no CRAMS found" >> $logfile
   exit 9
+else
+#   Clean out cruft from the Broad. For now the cruft is left in $dir/incoming
+  rm -rf incoming
 fi
-echo ""
-echo "SUCCESSFUL  Fetched $n $uri files to $ddndir/$dir" | tee -a $tmpf
+echo "SUCCESSFUL  Fetched $n $uri files to $ddndir/$dir"   >> $logfile
+echo "Manifest.txt shows `ls -l Manifest.txt` .cram files" >> $logfile
+echo `date` >> $logfile
+echo  ''    >> $logfile
+echo -n "$localdir	:  " >> $logfile
 
-echo "Creating Manifest.txt from the MD5 files" | tee -a $tmpf
-mf=Manifest.txt
-rm -f $mf
-for f in *.cram; do
-  md5=(`cat $f.md5`)
-  echo "${md5[2]}   $f" >> $mf
-done
-echo "Created in $ddndir/$dir/$mf" | tee -a $tmpf
-ls -l $mf
+#   Register this new run in the monitor database
+#   This part of the code is hard-wired with center = broad
+
+linkdir=`echo $dir | sed 's:/net::'`
+center=broad
+cd /net/topmed/incoming/topmed/$center
+ln -s ../../../../$linkdir .
+cd $HOME
+/usr/cluster/topmed/bin/topmed_init.pl -center $center updatedb >> $dir/$logfile
+echo '' >> $dir/$logfile
+exit $?
+
+
