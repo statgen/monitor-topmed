a2a8db2ba9284fe6a61f9c5495e5dfe71b8e208a
Terry Gliedt <tpg@hps.com>
2018-10-22 09:29:08 -0400

    Handle arguments better

--- bin/utils/where.sh
+++ bin/utils/where.sh
@@ -5,13 +5,13 @@
 bamid=$1
 fcn=bam
 pth=/usr/cluster/topmed/bin/topmedpath.pl
-if [ "$2" != "" ]; then
-  $pth wherefile $bamid $2
-  exit $? 
+shift;
+
+#  better handling of arguments 2,3,...  tb 9/7/2018 :
+
+if [ $# -ge 1 ]; then x=$*;
+else x="bam cram b38";
 fi
-#echo -n "FCN=$fcn  "
-echo -n 'bam   '; $pth wherefile $bamid bam
-echo -n 'cram  '; $pth wherefile $bamid cram
-echo -n 'b38   '; $pth wherefile $bamid b38
-echo -n 'b37   '; $pth wherefile $bamid b37
-echo ''
+for t in $x; do
+  printf '%-12s' $t; $pth wherefile $bamid $t
+done
