e2d7646dc7c7204802b5669aabdee8c92574da13
Terry Gliedt <tpg@hps.com>
2018-11-05 14:10:24 -0500

    Use QOS topmed-gce

--- bin/topmed_gcecpbcf.sh
+++ bin/topmed_gcecpbcf.sh
@@ -14,7 +14,7 @@ if [ "$1" = "-submit" ]; then
   RandomRealHost $bamid
   MayIRun $me $bamid $realhost
   timeout='2:00:00'
-  SubmitJob $bamid $PROJECT '3G' "$0 $*"
+  SubmitJob $bamid "$PROJECT-gce" '3G' "$0 $*"
   exit
 fi
 
